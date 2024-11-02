746251648db493d91ca0fdf6af8a05b37175e3b1
IsDevOps <issyoseghale@gmail.com>
2024-11-02 14:54:58 +0100

    commit

--- providers.tf
+++ providers.tf
@@ -1,18 +1,28 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 4.0.0"
     }
+    
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10.0"
+    }
+    
     kubectl = {
-      source           = "gavinbunney/kubectl"
-      version          = ">= 1.10.0"
-
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.5.1"
     }
   }
 }
-
 
 provider "aws" {
   region = var.region
