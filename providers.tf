ce18936c346b311696b90297be12cb9959c03949
IsDevOps <issyoseghale@gmail.com>
2024-11-02 14:25:12 +0100

    commit

--- providers.tf
+++ providers.tf
@@ -24,9 +24,9 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   region = var.region
-# }
+provider "aws" {
+  region = var.region
+}
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
@@ -42,7 +42,7 @@ provider "kubernetes" {
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
+  load_config_file       = false  # Ensures no kubeconfig conflicts
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -55,6 +55,7 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    load_config_file       = false  # Ensures no kubeconfig conflicts
     
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
