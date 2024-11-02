73366faa70e4146bef4b4c0395c419c55360df67
IsDevOps <issyoseghale@gmail.com>
2024-11-02 14:35:17 +0100

    commit

--- providers.tf
+++ providers.tf
@@ -1,22 +1,15 @@
 terraform {
   required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0.0"
+      version = ">= 4.0"
     }
-    
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10.0"
-    }
-    
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
+      source           = "gavinbunney/kubectl"
+      version          = ">= 1.14.0"
+
     }
-    
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.5.1"
