cdcae7be4c9bc348dccfa42d6e40be9493b21085
IsDevOps <issyoseghale@gmail.com>
2024-11-02 14:14:32 +0100

    commit

--- providers.tf
+++ providers.tf
@@ -24,9 +24,9 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
